5d5ad4c5de94176eeaa75fb07bf5a06ea9c7fc48
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 20:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ function findMatching(drivers, string) {
   return drivers.filter(function (name) {
     return name.toLowerCase() === string.toLowerCase()
   })
-  }
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(function (name) {
+    return name === string
+  })
+}
