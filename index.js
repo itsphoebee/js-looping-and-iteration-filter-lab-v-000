e0afb6ad5e83b7644ae960f439b1c8df13239f7f
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 19:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(collection, string) {
-  collection.filter(function(name) { return name === string })
+  collection.filter(name { return name === string })
   }
