b548f22ab26061310033a733818d99cdef2c73c7
Phoebe Lau <phoebelau7@gmail.com>
2018-03-14 19:57:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(collection, string) {
-  drivers.filter(function(name) { return name === string })
+  collection.filter(function(name) { return name === string })
   }
